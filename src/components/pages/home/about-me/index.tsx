257a74f57435e22a43e57215794248e6b1989b1d
Thomasdevsp <thomasdevsp@gmail.com>
2025-08-21 08:33:17 -0300

    feat: adding text for me

--- src/components/pages/home/about-me/index.tsx
+++ src/components/pages/home/about-me/index.tsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
   return (
     <Stack
       sx={{
-        padding: isMobile ? "1.5rem" : "5rem 0",
+        padding: isMobile ? "3rem 1.5rem" : "5rem 0",
 
         alignItems: "center",
         gap: isMobile ? "2rem" : "5rem",
@@ -16,7 +16,7 @@ export default function AboutMe() {
       <Stack
         sx={{
           alignItems: "center",
-          gap: "1.125rem",
+          textAlign: "center",
         }}
       >
         <Typography
@@ -78,30 +78,43 @@ export default function AboutMe() {
           }}
         >
           <Typography>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti earum
-            nobis, cumque error excepturi asperiores magni dolores aut illo quae doloribus,
-            iusto in provident suscipit sed voluptas magnam fugit mollitia.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti earum
-            nobis, cumque error excepturi asperiores magni dolores aut illo quae doloribus,
-            iusto in provident suscipit sed voluptas magnam fugit mollitia.
+            <b>Desenvolvedor Front-end | Next.js | TypeScript | Material UI</b>
+            <br /><br />
+            Atualmente, sou responsável pelo desenvolvimento e manutenção de websites na Summus
+            Marketing, com foco em plataformas para grandes eventos como o São Paulo Oktoberfest,
+            Villa de Natal SP, Expoflora e Natal de Holambra. Minha atuação é centrada na criação de
+            novas funcionalidades e na otimização das plataformas, garantindo que os sites estejam
+            sempre em seu melhor desempenho e visual.
+          </Typography>
+
+          <Typography
+            sx={{
+              "a": {
+                color: "#CBACF9",
+                textDecoration: "none",
+
+              }
+            }}
+          >
+            Minha paixão por tecnologia começou na infância com jogos, mas foi na pré-adolescência,
+            após montar meu primeiro computador e ser influenciado fortemente pelo meu irmão
+            <a href="https://www.vitorsampaio.com.br/" target="_blank"> @vitor</a>, que me
+            aventurei na lógica de programação. Essa curiosidade me levou a cursar o ensino médio
+            técnico no UNASP, onde aprofundei meus conhecimentos em desenvolvimento web. Durante
+            essa jornada, também busquei cursos por conta própria para expandir minhas habilidades,
+            e confesso que minhas experiências na adolescência, criando canais no YouTube, me deram
+            uma base sólida em edição de foto e vídeo, facilitando o aprendizado de conceitos de
+            inovação e criatividade.
           </Typography>
 
           <Typography>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti earum
-            nobis, cumque error excepturi asperiores magni dolores aut illo quae doloribus,
-            iusto in provident suscipit sed voluptas magnam fugit mollitia.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti earum
-            nobis, cumque error excepturi asperiores magni dolores aut illo quae doloribus,
-            iusto in provident suscipit sed voluptas magnam fugit mollitia.
-          </Typography>
+            Essa jornada me preparou para o desenvolvimento web. Na VTT, atuei como desenvolvedor
+            front-end, colaborando na construção e evolução da interface de um produto. Além de
+            implementar interfaces modernas e responsivas e cultivar uma amizade e espécie de
+            &quot;apadrinhamento&quot; com o estagiário  que estava guiando no mundo da programação,
+            também participei ativamente dos planejamentos, contribuindo com ideias e
+            melhorias.
 
-          <Typography>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti earum
-            nobis, cumque error excepturi asperiores magni dolores aut illo quae doloribus,
-            iusto in provident suscipit sed voluptas magnam fugit mollitia.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti earum
-            nobis, cumque error excepturi asperiores magni dolores aut illo quae doloribus,
-            iusto in provident suscipit sed voluptas magnam fugit mollitia.
           </Typography>
         </Stack>
       </Stack>
