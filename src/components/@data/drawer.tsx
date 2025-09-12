d2d97b477cd9cfa3116ee23af68f5fb78269d6e8
Thomasdevsp <thomasdevsp@gmail.com>
2025-09-12 17:39:14 -0300

    refactor: removing useless things

--- src/components/@data/drawer.tsx
+++
@@ -1,5 +0,0 @@
-import Blog from "../pages/blog";
-
-export const data_drawer = {
-  "da-curiosidade-a-carreira": ({ handleClose }) => <Blog onClose={handleClose} />,
-};
--- src/lib/constants.ts
+++
@@ -1,7 +0,0 @@
-export const IMAGE_HOST_PREFIX = `https://${process.env.NEXT_PUBLIC_S3_BUCKET}.s3.${process.env.NEXT_PUBLIC_AWS_REGION}.amazonaws.com`;
-
-export function getPublicS3UrlWithAlternative(endpoint: string | null | undefined, alternative: string): string {
-   if (typeof endpoint !== 'string') return alternative;
-   let url = new URL(IMAGE_HOST_PREFIX + '/' + endpoint);
-   return url.toString();
-}
